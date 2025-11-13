94e23c2768ac632ba6a0d691e5750d9464a8f129
Afraz Mishak <afrazmishak@gmail.com>
2025-11-13 07:35:12 +0400

    Zikr Button cretion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import './App.css'
 
+function Astagfirullah() {
+    return (
+    <button className="button-counter" onClick={ZikrButton}>
+        <p className="line-one-arabic">أستغفر الله</p>
+        <span className="line-one-english">"I ask God for forgiveness"</span>
+    </button>
+    )
+}
+
+function Count() {
+    return (
+        <p>Count</p>
+    )
+}
+
 function App() {
-  
+  return (
+    <>
+      <Astagfirullah />
+      <Count />
+    </>
+  )
 }
 
 export default App
